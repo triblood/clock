e7af80f9f3bbef79bdc790743bc0365c5dbad167
Davi Camarinha <davi.camarinha@agenciaecan.com.br>
2018-05-15 12:25:15 -0300

    Usando milisegunds

--- js/script.js
+++ js/script.js
@@ -3,40 +3,50 @@ $(document).ready(function(){
     var hora = now.getHours();
     var minuto = now.getMinutes();
     var segundo = now.getSeconds();
+    var mili = 0;
 
     // hora = 23;
     // minuto = 59;
     // segundo = 50;
-
+    
     setInterval(function() {
-        var horaDeg = ((hora/12) * 360)+75;
-        var minDeg = ((minuto/60) * 360)+75;
-        var segDeg = ((segundo/60) * 360)+75;
+        var segM = parseFloat(((segundo * 1000) + mili) / 1000);
+        var minM = parseFloat(((minuto * 60) + segM) / 60);
+        var horaM = parseFloat(((hora * 60) + minM) / 60)
+        var horaDeg = ((horaM/12) * 360);
+        var minDeg = ((minM/60) * 360);
+        var segDeg = ((segM/60) * 360);
 
         $('.horas').css('transform','rotate('+horaDeg+'deg)');
         $('.minutos').css('transform','rotate('+minDeg+'deg)');
         $('.segundos').css('transform','rotate('+segDeg+'deg)');
 
-        if((minuto >= 59) && (segundo >= 59)){
-            hora = hora + 1
-        }
+        if(mili == 1000){
+            if((minuto >= 59) && (segundo >= 59)){
+                hora = hora + 1
+            }
 
-        if((hora >= 23) && (minuto == 59) && (segundo == 59)){
-            hora = 0;
-        }
+            if((hora >= 23) && (minuto == 59) && (segundo == 59)){
+                hora = 0;
+            }
 
-        if(segundo >= 59){
-            minuto = minuto + 1
-        }
+            if(segundo >= 59){
+                minuto = minuto + 1
+            }
 
-        if(segundo < 59){
-            segundo = segundo + 1;
+            if(segundo < 59){
+                segundo = segundo + 1;
+            } else {
+                segundo = 0;
+            }
+
+            if(minuto > 59){
+                minuto = 0;
+            }
+
+            mili = 0;
         } else {
-            segundo = 0;
+            mili = mili + 10;  
         }
-
-        if(minuto > 59){
-            minuto = 0;
-        }
-    }, 1000);
+    }, 10);
 });
